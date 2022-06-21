c22063493ef35d60084057980a074bece9c4795c
codergalib <coderboygalif@gmail.com>
2022-06-21 21:29:16 +0600

    commited

--- src/routes/products.js
+++ src/routes/products.js
@@ -16,5 +16,19 @@ product.get("/products", async (req, res) => {
     });
   }
 });
+// Single Product Load
+product.get("/:id", async (req, res) => {
+  try {
+    const data = await Product.find({ _id: req.params.id });
+    res.status(200).json({
+      data,
+      message: "Single product load!!",
+    });
+  } catch (err) {
+    res.status(403).json({
+      error: err.message,
+    });
+  }
+});
 
 module.exports = product;
